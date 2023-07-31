faf48019d184e72a87815959d7235e429b92ecbc
mmmp <hadyanap@gmail.com>
2023-07-31 14:36:05 +0700

    migration models

---
+++ projects/server/models/category.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('DataTypes');
+module.exports = (DataTypes, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of DataTypes lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.Products, {foreignKey: 'categoryId'})
+    }
+  }
+  Category.init({
+    categoryName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {
+    DataTypes,
+    modelName: 'Category',
+  });
+  return Category;
+};
