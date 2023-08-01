50ed1dfe39087cf4538a3b9d9d605a4033243249
Gian Septian Lattang <gianseptianlattang@gmail.com>
2023-08-01 23:44:24 +0700

    change to logo

--- projects/client/src/components/order/Order.jsx
+++ projects/client/src/components/order/Order.jsx
@@ -1,38 +1,46 @@
 // OrderPage.js
 import React from "react";
-import { Box, Flex, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Icon,
+  Image,
+  List,
+  ListItem,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { GoHome } from "react-icons/go";
+import {
+  IoFastFoodOutline,
+  IoSettingsOutline,
+  IoLogOutOutline,
+} from "react-icons/io5";
 
 export const Order = () => {
   return (
     <Flex>
-      <Box w="100px" h="100vh" py={4} px={2}>
-        <List spacing={2} pl={4}>
-          <ListItem>
-            <Text fontWeight="bold" fontSize="lg">
-              HOME
-            </Text>
-          </ListItem>
-          <ListItem>
-            <Text fontWeight="bold" fontSize="lg">
-              MENU
-            </Text>
-          </ListItem>
-          <ListItem>
-            <Text fontWeight="bold" fontSize="lg">
-              REPORT
-            </Text>
-          </ListItem>
-          <ListItem>
-            <Text fontWeight="bold" fontSize="lg">
-              SETTING
-            </Text>
-          </ListItem>
-          <ListItem>
-            <Text fontWeight="bold" fontSize="lg">
-              LOGOUT
-            </Text>
-          </ListItem>
-        </List>
+      <Box w="120px" h="100vh" pb="50px">
+        <Flex h="full" flexDirection="column">
+          <Image pb={"40px"} src="/image/foodlogo.png" />
+          <List spacing="20px">
+            <ListItem>
+              <Icon as={GoHome} boxSize={10} />
+            </ListItem>
+            <ListItem>
+              <Icon as={IoFastFoodOutline} boxSize={10} />
+            </ListItem>
+          </List>
+          <Spacer />
+          <List spacing="20px">
+            <ListItem>
+              <Icon as={IoSettingsOutline} boxSize={10} />
+            </ListItem>
+            <ListItem>
+              <Icon as={IoLogOutOutline} boxSize={10} />
+            </ListItem>
+          </List>
+        </Flex>
       </Box>
 
       <Box bg="gray.100" borderWidth={"1px"} flex={3} p={4}>
