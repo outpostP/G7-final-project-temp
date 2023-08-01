43b33e26ec1a2fe6c41a254deb9f704700c27d5b
mmmp <hadyanap@gmail.com>
2023-08-01 14:12:12 +0700

    fix controllers routers

---
+++ projects/server/src/routers/adminRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {getCategory, getCashier, addCategory, addCashier, getProduct, addProduct, updateCategory} = require('../controller/admin/ctrlAdmin');
+const {multerUpload} = require('../middleware/multer')
+
+
+router.get('/cate', getCategory);
+router.post('/cate', addCategory);
+router.patch('/cate', updateCategory);
+router.get('/cashier', getCashier);
+router.post('/cashier', addCashier);
+router.get('/product', getProduct);
+router.post('/product', multerUpload.single('productImage') ,addProduct);
+
+
+module.exports = router;
