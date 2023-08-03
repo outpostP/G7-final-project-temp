c16ab0542f96e00157102273d907ad928b5809a7
Gian Septian Lattang <gianseptianlattang@gmail.com>
2023-08-03 11:30:18 +0700

    not used sidebar on loginpage

--- projects/client/src/Layout/Homepage.jsx
+++ projects/client/src/Layout/Homepage.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import {Box} from '@chakra-ui/react';
-
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import { Box } from "@chakra-ui/react";
+const isLogin = localStorage.getItem("token");
 const Homepage = () => {
   return (
     <Box display="flex">
-      <Sidebar />
+      {isLogin && <Sidebar />}
       <Box flex="1">
         <Outlet />
       </Box>
