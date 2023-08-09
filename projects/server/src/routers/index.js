e2b58b4084e1676edacb6656f313e7825805066d
Gian Septian Lattang <gianseptianlattang@gmail.com>
2023-08-10 03:06:03 +0700

    change router back

--- projects/server/src/routers/index.js
+++ projects/server/src/routers/index.js
@@ -1,11 +1,9 @@
 const authRouter = require("./authRouter");
 const profileRouter = require("./profileRouter");
 const adminRouter = require("./adminRouter");
-const cashierRouter = require("./cashierRouter");
 
 module.exports = {
   authRouter,
   profileRouter,
   adminRouter,
-  cashierRouter,
 };
